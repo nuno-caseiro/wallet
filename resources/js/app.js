17289a40bc8d4740e76da7726a8d89d67dbad80f
Nuno Caseiro <2171560@my.ipleiria.pt>
2019-11-18 17:57:34 +0000

    navbar

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,24 @@
 require('./bootstrap');
+import BootstrapVue from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 window.Vue = require('vue');
 
+Vue.use(BootstrapVue);
+
+import NavbarComponent from './components/navbar.vue';
+const navbar= Vue.component('navbar',NavbarComponent);
+
 
 const app = new Vue({
+    data:{
 
+
+    },
+    components:{
+        navbar
+    }
 
 }).$mount('#app');
 
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,21 +2,20 @@
 <html lang="{{ config('app.locale') }}">
 
 <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>@yield('title')</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<!-- Latest compiled and minified CSS & JS -->
-    <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}">
+
 </head>
 
 <body>
+
 <div class="container" id="app">
 
+    <navbar></navbar>
+
+
 </div>
+
 <script src="js/app.js"></script>
+
 </body>
 
 </html>
@@ -138,4 +137,11 @@
 </div>
 </body>
 </html>
+
+<script>
+    import Navbar from "../js/components/navbar";
+    export default {
+        components: {Navbar}
+    }
+</script>
 --}}
