67b6e00f861d8b6e1da086b21273ac2641fae45d
Nuno Caseiro <2171560@my.ipleiria.pt>
2019-11-20 22:57:02 +0000

    wallet routes

--- routes/api.php
+++ routes/api.php
@@ -18,22 +18,29 @@ use Illuminate\Http\Request;
 });
 */
 Route::middleware('auth:api')->group(function () {
+    Route::post('logout', 'LoginControllerAPI@logout');
+
+    Route::post('users','UserControllerAPI@store');
 
     //TODO proteger para apenas administrador
     Route::get('users','UserControllerAPI@index');
-    Route::post('users','UserControllerAPI@store');
     Route::delete('users/{id}','UserControllerAPI@destroy');
-    //
 
+
+    //TODO apenas proprio user ou admin
     Route::get('users/{id}', 'UserControllerAPI@show');
+
+    //TODO apenas o proprio user
     Route::put('users/{id}', 'UserControllerAPI@update');
+
+    Route::get('wallets', 'WalletControllerAPI@index');
+    Route::post('wallets', 'WalletControllerAPI@store');
+    Route::put('wallets/{id}', 'WalletControllerAPI@update');
+    Route::delete('wallets/{id}', 'WalletControllerAPI@delete');
 
 
 });
 
-
-
-
 Route::post('login', 'LoginControllerAPI@login')->name('login');
 Route::post('register', 'RegisterControllerAPI@register')->name('register'); //TODO
-Route::middleware('auth:api')->post('logout', 'LoginControllerAPI@logout');
+//Route::middleware('auth:api')->post('logout', 'LoginControllerAPI@logout');
