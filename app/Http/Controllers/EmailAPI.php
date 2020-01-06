f4b677de02c87472abae514bdb26e61ffd805a3f
Nuno Caseiro <nunocas3iro@gmail.com>
2020-01-06 15:50:11 +0000

    no message

--- app/Http/Controllers/EmailAPI.php
+++ app/Http/Controllers/EmailAPI.php
@@ -11,12 +11,14 @@ class EmailAPI extends Controller
     {
 
         if($request->has('msg') && $request->has('sourceUser') && $request->has('destUser') ){
+            if(auth()->guard('api')->user()->id==$request->sourceUser['id']){
             Mail::raw($request->msg, function ($message) use ($request) {
                 $message->subject('Virtual Wallet: income');
                 $message->from($request->sourceUser['email']);
                 $message->to($request->destUser['email']);
                 //$message->to('nunocas3iro@gmail.com');
             });
+            }
         }
         return response()->json(['message'=>'Email sent']);
 
